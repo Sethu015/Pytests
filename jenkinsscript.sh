84dd744d3f71bfc1cb0c68d68fdb7a12db6ecd58
Sethu015 <88558500+Sethu015@users.noreply.github.com>
2023-01-14 23:38:36 +0530

    Update jenkinsscript.sh

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 . ~/.bashrc
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-if command -v pyenv 1>/dev/null 2>&1; then
- eval "$(pyenv init --path)"
-fi
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 pyenv versions
 
